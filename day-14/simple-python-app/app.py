fa26d94b93b9db3ec9979f8f948d4cce54cbe990
karthik3491 <karthikitsm91@gmail.com>
2025-04-26 22:46:48 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Karthik and Nila!'
+    return 'Hello, Karthik, Nila and Viyanya !'
 
 if __name__ == '__main__':
     app.run()
